e7af3d19aa25f49bff3ee5bb9f360e10dac00480
Nikita <nik0954783486@gmail.com>
2023-01-23 16:40:09 +0200

    Added count of words

--- viceversa/view.py
+++ viceversa/view.py
@@ -8,6 +8,8 @@ def home(request):
 
 def reverse(request):
     user_text = request.GET['usertext']
+    len_of_text = len(user_text.split())
     reverse_user_text = user_text[::-1]
     return render(request, 'reverse.html', {'user_text': user_text,
-                                            'reverse_user_text': reverse_user_text})
+                                            'reverse_user_text': reverse_user_text,
+                                            'len_of_text': len_of_text})
